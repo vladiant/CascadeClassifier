587d15c65fc77a375d989504d514ec364f3401eb
Vladislav Antonov <vladislav.antonov@gmail.com>
2022-02-05 00:01:49 +0200

    Fix Python detect tool

--- tools/detection/Python/detect.py
+++ tools/detection/Python/detect.py
@@ -1,15 +1,25 @@
 # https://pythonprogramming.net/haar-cascade-object-detection-python-opencv-tutorial/
+
+import argparse as ap
+
 import numpy as np
 import cv2
 
-watch_cascade = cv2.CascadeClassifier("cascade.xml")
+parser = ap.ArgumentParser()
+parser.add_argument("-d", "--descriptor", help="cascade descriptor XML file", required="True")
+parser.add_argument("-i", "--image", help="image filename", required="True")
+args = vars(parser.parse_args())
 
-img = cv2.imread("airplane_001.jpg", cv2.IMREAD_COLOR)
+descriptor_path = args["descriptor"]
+object_cascade = cv2.CascadeClassifier(descriptor_path)
+
+image_path = args["image"]
+img = cv2.imread(image_path, cv2.IMREAD_COLOR)
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-watches = watch_cascade.detectMultiScale(gray, 4, 50)
+objects = object_cascade.detectMultiScale(gray, 4, 50)
 
-for (x, y, w, h) in watches:
+for (x, y, w, h) in objects:
     cv2.rectangle(img, (x, y), (x + w, y + h), (255, 255, 0), 2)
 
 cv2.imshow("img", img)
